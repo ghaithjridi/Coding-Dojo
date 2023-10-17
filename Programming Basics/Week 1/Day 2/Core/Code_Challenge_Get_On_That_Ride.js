51f00014f768dd375b1518aa0e20fd5270487d58
ghaithjridi <ghaithjridi5@gmail.com>
2023-10-17 16:56:45 +0100

    commit

---
+++ Programming Basics/Week 1/Day 2/Core/Code_Challenge_Get_On_That_Ride.js
@@ -0,0 +1,29 @@
+var height = 40
+if ( height >= 42)
+{
+
+    console.log("Get on that ride, kiddo!")
+
+} 
+else {
+    console.log("Sorry kiddo. Maybe next year.")
+
+}
+
+var height = 40
+var age= 10
+if (height>=42 && age >=10 )
+{
+    console.log("Get on that ride, kiddo!")  
+}
+else
+{
+    console.log("Sorry kiddo. Maybe next year.")
+}
+
+var height = 40
+var age= 10
+if (height>= 42 || age >= 10 )
+{
+    console.log("Get on that ride, kiddo!")  
+}
---
+++ Programming Basics/Week 1/Day 2/Optional/code.js
@@ -0,0 +1,7 @@
+// var likeCount is set to equal 0
+var likeCount = 0;
+// icreaseLikes
+function increaseLikes() {
+    // is set to be likeCount equal to likecount plus 1
+    likeCount = likeCount + 1;
+}
