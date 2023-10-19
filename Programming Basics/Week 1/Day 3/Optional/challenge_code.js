5b2cdb1a8844adc1751d5050bb95242d1f88c421
ghaithjridi <ghaithjridi5@gmail.com>
2023-10-19 09:20:44 +0100

    commit

---
+++ Programming Basics/Week 1/Day 3/Optional/challenge_code.js
@@ -0,0 +1,23 @@
+var numberOfPieces = 12  
+var numberOfPeople = 5
+var I= numberOfPieces % numberOfPeople
+howMuchLeftOverCake(numberOfPieces,numberOfPeople,remainigpieces)
+
+function howMuchLeftOverCake (numberOfPieces, numberOfPeople) {
+for ( var i = 0, i<=12 , i=i+1);{
+
+
+if(  i === 0 ){
+console.log("No leftovers for you!")
+}
+else if( i<=2){
+console.log("You have some leftovers")
+}
+else if( i<=3){
+console.log("You have leftovers to share")
+}
+else if( i >=5) {
+console.log("Hold another party!")
+}
+}
+}
---
+++ Programming Basics/Week 1/Day 3/Practice/code_challenge_parametr.js
@@ -0,0 +1,17 @@
+
+// function greetSomeone (person){
+//     if (person ==  "Anakin" ) {
+//         console.log("Good day, Anakin");
+//     }
+// }
+// greetSomeone ("Anakin")
+
+var user= "ghaith"
+var user1 ="Dooku"
+countDooku(user,user1)
+function countDooku(user,user1) {
+if (user != user1 ) {
+console.log("I'm coming for you, Dooku!" )
+}
+countDooku(user,user1)
+}
