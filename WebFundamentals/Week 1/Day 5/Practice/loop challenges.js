d2f03166d6dc7fb1b243fce5d7761ef9991d7d4b
ghaithjridi <ghaithjridi5@gmail.com>
2023-10-30 09:58:37 +0100

    commit

---
+++ WebFundamentals/Week 1/Day 5/Practice/loop challenges.js
@@ -0,0 +1,28 @@
+// function get_array(){
+//  var arr=[];
+// for (var i =1 ;i<=20;i++){
+//     arr.push(i)
+// }
+// return arr;
+// // }
+
+
+    var arr =[];
+for(var i=100;i=>0,i--);{
+    if(i % 3===0 );{
+        console.log(i)
+    }
+
+}
+var sum=0
+for(var i = 1;i<=100;i++){
+    sum=sum+i
+
+console.log(sum)
+}
+var product=1
+for(var i=1;i<=12;i++){
+    product=product*i
+    
+}
+console.log(product)
