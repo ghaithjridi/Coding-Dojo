37f0c79cb9bdea86a1a407a11dcc29d235208c76
ghaithjridi <ghaithjridi5@gmail.com>
2023-11-13 22:35:18 +0100

    commit

--- Python/Week 1/Day 1/Practice/test.py
+++ Python/Week 1/Day 1/Practice/test.py
@@ -1,18 +1,28 @@
-print('Welcome to Python!')
+# print('Welcome to Python!')
 
-print('This is a loop printing 5 times')
-for x in range(1, 6):
-    print(f'x is: {x}')
+# print('This is a loop printing 5 times')
+# for x in range(1, 6):
+#     print(f'x is: {x}')
 
-weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-day = random.choice(weekdays)
+# weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+# day = random.choice(weekdays)
 
-print(f'Today is: {day}')
+# print(f'Today is: {day}')
 
-if day == 'Monday':
-    print('It will be a long week!')
-elif(day == 'Friday'):
-    print('Woohoo, time for the weekend!')
-else:
-    print('Not quite there yet.')
+# if day == 'Monday':
+#     print('It will be a long week!')
+# elif(day == 'Friday'):
+#     print('Woohoo, time for the weekend!')
+# else:
+#     print('Not quite there yet.')
     
+def multiply(num_list, num):
+    for x in num_list:
+        x *= num
+    return num_list
+a = [2,4,10,16]
+b = multiply(a,5)
+print(b)
+# # output:
+# >>>[2,4,10,16]
+
