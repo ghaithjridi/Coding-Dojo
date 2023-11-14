ed0c9632a8d60f7c80ba4cf0ed52fe2698b72534
ghaithjridi <ghaithjridi5@gmail.com>
2023-11-14 23:26:33 +0100

    commit

---
+++ Python/Week 1/Day 2/Practice/chaining methods.py
@@ -0,0 +1,41 @@
+# declare a class and give it name User
+class User:		
+    def __init__(self, first_name, last_name, email, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email=email        
+        self.age = age
+        self.is_rewards_member=False
+        self.gold_card_points = 0
+
+    def display_info(self):
+        print(f"Hello, my name is {self.first_name} {self.last_name} {self.email} {self.age} {self.is_rewards_member} {self.gold_card_points}")
+    def enroll(self):
+        self.is_rewards_member = True
+        self.gold_card_points = 200
+    def spend_points(self, amount):
+        self.gold_card_points-=amount
+        
+        
+        
+user = User("ghaith","jridi","ghaithjridi5@gmail.com",24)
+user.display_info()
+user2=User("ghzela","hajji","ghzelahajji@gmail.com",55)
+user3=User("bilel","jridi","bileljridi@gmail.com",34)
+user.spend_points(50)
+user2.enroll
+user2.spend_points(80)
+
+user.display_info
+user2.display_info
+user3.display_info
+
+
+
+
+        
+        
+
+        
+
+        
