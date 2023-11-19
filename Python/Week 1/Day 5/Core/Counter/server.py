3da2f40ceded4f21d15e53f72eb6c7e0cdf8090f
ghaithjridi <ghaithjridi5@gmail.com>
2023-11-19 15:48:17 +0100

    commit

---
+++ Python/Week 1/Day 5/Core/Counter/server.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template,redirect,session
+app = Flask(__name__)
+app.secret_key='lkjhfdfsdfs'
+# our index route will handle rendering our form
+
+@app.route('/')
+def index():
+    if 'count' not in session:
+        session['count']=0
+    session['count']+=1
+    return render_template("index.html", count= session['count'])
+
+@app.route('/destroy_session')
+
+def addtwo():
+    session['count']+=2
+    return redirect('/')
+        
+@app.route('/reset')
+def reset():
+    session['count']=0
+    return redirect('/')
+        
+    
+    
+    
+    
+
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
