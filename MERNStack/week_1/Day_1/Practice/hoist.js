741ba881d203b0bfceb727c9bb5cb10e1a4407c7
ghaithjridi <ghaithjridi5@gmail.com>
2023-12-18 18:24:30 +0100

    commit

---
+++ MERNStack/week_1/Day_1/Practice/hoist.js
@@ -0,0 +1,86 @@
+// var hello = 'world';
+// console.log(hello); 
+// hello world
+
+// var needle = 'haystack';
+// test();
+// function test() {
+//     var needle = 'magnet';
+//     console.log(needle);
+// }
+// magnet
+// var brendan = 'super cool';
+// function print() {
+//     brendan = 'only okay';
+//     console.log(brendan);
+// }
+// console.log(brendan);
+// super cool
+
+// var food = 'chicken';
+// console.log(food);
+// eat();
+// function eat() {
+//     food = 'half-chicken';
+//     console.log(food);
+//     var food = 'gone';
+// }
+// chicken
+// half-chicken
+
+
+
+// console.log(genre);
+// var genre = "disco";
+// rewind();
+// function rewind() {
+//     genre = "rock";
+//     console.log(genre);
+//     var genre = "r&b";
+//     console.log(genre);
+// }
+// console.log(genre);
+// "rock"
+// "r&b"
+// "disco"
+
+
+// dojo = "san jose";
+// console.log(dojo);
+// learn();
+// function learn() {
+//     dojo = "seattle";
+//     console.log(dojo);
+//     var dojo = "burbank";
+//     console.log(dojo);
+// }
+// console.log(dojo);
+// "san jose"
+// "seattle"
+// "burbank"
+// "san jose"
+
+
+// console.log(makeDojo("Chicago", 65));
+// console.log(makeDojo("Berkeley", 0));
+// function makeDojo(name, students) {
+//     let dojo = {};
+//     dojo.name = name;
+//     dojo.students = students;
+//     if (dojo.students > 50) {
+//         dojo.hiring = true;
+//     }
+//     else if (dojo.students <= 0) {
+//         dojo = "closed for now";
+//     }
+//     return dojo;
+// }
+// {name:'chicago,students:65,hiring:true'}
+// closed for now
+
+
+
+
+
+
+
