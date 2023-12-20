25f1fded21c900d2507c145bf583fad8f6d08f50
ghaithjridi <ghaithjridi5@gmail.com>
2023-12-20 18:51:36 +0100

    commit

---
+++ MERNStack/week_1/Day_3/Core/first_jsx/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hello Dojo</h1>
+    <h3>Things i need to do:</h3>
+    <li>learn react</li>
+    <li>Climb Mt.Everest</li>
+    <li>Run a marathon</li>
+    <li>feed the dogs</li>
+    </div>
+  );
+}
+
+export default App;
