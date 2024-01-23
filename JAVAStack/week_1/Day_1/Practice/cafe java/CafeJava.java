66a045336fae8ae9c83cf96bd4cc753e9ee9dbe6
ghaithjridi <ghaithjridi5@gmail.com>
2024-01-23 18:37:34 +0100

    commit

---
+++ JAVAStack/week_1/Day_1/Practice/cafe java/CafeJava.java
@@ -0,0 +1,111 @@
+// public class CafeJava {
+//     public static void main(String[] args) {
+//         // APP VARIABLES
+//         // Lines of text that will appear in the app. 
+//         String generalGreeting = "Welcome to Cafe Java, ";
+//         String pendingMessage = ", your order will be ready shortly";
+//         String readyMessage = ", your order is ready";
+//         String displayTotalMessage = "Your total is $";
+        
+//         // Menu variables (add yours below)
+//         double dripCoffe = 3.5;
+//         double latteCoffe = 4.5;
+//         double cappucinoCoffe = 5.5;
+    
+    
+    
+//         // Customer name variables (add yours below)
+//         String customer1 = "Cindhuri";
+    
+//         // Order completions (add yours below)
+//         boolean statusFlag = true;
+    
+//         // APP INTERACTION SIMULATION (Add your code for the challenges below)
+//         // Example:
+//         System.out.println(statusFlag ? "your order is ready " : " your order will be ready shortly" ); // Displays "Welcome to Cafe Java, Cindhuri"
+//     	// ** Your customer interaction print statements will go here ** //
+//     }
+// }
+
+// public class CafeJava {
+//     public static void main(String[] args) {
+//         // APP VARIABLES
+//         // Lines of text that will appear in the app. 
+//         String generalGreeting = "Welcome to Cafe Java, ";
+//         String pendingMessage = ", your order will be ready shortly";
+//         String readyMessage = ", your order is ready";
+//         String displayTotalMessage = "Your total is $";
+        
+//         // Menu variables (add yours below)
+//         double mochaPrice = 3.5;
+//         double cappucinoCoffe = 5.5;
+//         double latteCoffe = 4.5;
+    
+//         // Customer name variables (add yours below)
+//         String customer1 = "Noah";
+    
+//         // Order completions (add yours below)
+//         boolean statusFlag = true;
+    
+//         // APP INTERACTION SIMULATION (Add your code for the challenges below)
+//         // Example:
+//         System.out.println(statusFlag ? readyMessage +displayTotalMessage+cappucinoCoffe :pendingMessage); // Displays "Welcome to Cafe Java, Cindhuri"
+//     	// ** Your customer interaction print statements will go here ** //
+//     }
+// }
+
+
+// public class CafeJava {
+//     public static void main(String[] args) {
+//         // APP VARIABLES
+//         // Lines of text that will appear in the app. 
+//         String generalGreeting = "Welcome to Cafe Java, ";
+//         String pendingMessage = ", your order will be ready shortly";
+//         String readyMessage = ", your order is ready";
+//         String displayTotalMessage = "Your total is $";
+        
+//         // Menu variables (add yours below)
+//         double mochaPrice = 3.5;
+//         double cappucinoCoffe = 5.5;
+//         double latteCoffe = 4.5;
+    
+//         // Customer name variables (add yours below)
+//         String customer1 = "Sam";
+    
+//         // Order completions (add yours below)
+//         boolean statusFlag = false;
+    
+//         // APP INTERACTION SIMULATION (Add your code for the challenges below)
+//         // Example:
+//         System.out.println(statusFlag ? readyMessage+displayTotalMessage +latteCoffe*2:pendingMessage ); // Displays "Welcome to Cafe Java, Cindhuri"
+//     	// ** Your customer interaction print statements will go here ** //
+//     }
+// }
+
+
+public class CafeJava {
+    public static void main(String[] args) {
+        // APP VARIABLES
+        // Lines of text that will appear in the app. 
+        String generalGreeting = "Welcome to Cafe Java, ";
+        String pendingMessage = ", your order will be ready shortly";
+        String readyMessage = ", your order is ready";
+        String displayTotalMessage = "Your total is $";
+        
+        // Menu variables (add yours below)
+        double mochaPrice = 3.5;
+        double cappucinoCoffe = 5.5;
+        double latteCoffe = 6.5;
+    
+        // Customer name variables (add yours below)
+        String customer1 = "Sam";
+    
+        // Order completions (add yours below)
+        boolean statusFlag = true;
+    
+        // APP INTERACTION SIMULATION (Add your code for the challenges below)
+        // Example:
+        System.out.println(statusFlag ? displayTotalMessage+(latteCoffe-cappucinoCoffe):pendingMessage ); 
+    	// ** Your customer interaction print statements will go here ** //
+    }
+}
--- JAVAStack/week_1/Day_1/Practice/first java program/Test.java
+++ JAVAStack/week_1/Day_1/Practice/first java program/Test.java
@@ -3,6 +3,7 @@ public class Test {
         // System.out.println("My name is coding dojo ");
         // System.out.println("i am 100 years old ");
         System.out.println("i am 100 years old ");
+        
 
     }
 }
