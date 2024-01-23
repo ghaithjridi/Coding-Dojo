61957bc8cf05a6460b3277ce508d7a0eb0047156
ghaithjridi <ghaithjridi5@gmail.com>
2024-01-23 22:11:34 +0100

    commit

--- JAVAStack/week_1/Day_1/Practice/AlfredBot/AlfredQuotes.java
+++ JAVAStack/week_1/Day_1/Practice/AlfredBot/AlfredQuotes.java
@@ -2,23 +2,23 @@ import java.util.Date;
 public class AlfredQuotes {
     
     public String basicGreeting() {
-        // You do not need to code here, this is an example method
+        
         return "Hello, lovely to see you. How are you?";
     }
     
     public String guestGreeting(String name, String dayPeriod) {
-        // YOUR CODE HERE
+        
         return String.format("Good %s, %s. Lovely to see you.", dayPeriod, name);
     }
     
     public String dateAnnouncement() {
-        // YOUR CODE HERE
+    
     
         return String.format("It is currently %s", new Date());
     }
     
     public String respondBeforeAlexis(String conversation) {
-        // YOUR CODE HERE
+    
         if(conversation.indexOf("Alexis") > -1) {
             return "She's really of no help. What can I get for you?";
         }
