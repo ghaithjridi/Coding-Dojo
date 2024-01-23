ffc995bc554cf5260c44e0069856db7cee78ba63
ghaithjridi <ghaithjridi5@gmail.com>
2024-01-23 15:06:42 +0100

    commit

--- JAVAStack/week_1/Day_1/Practice/Test.java
+++ JAVAStack/week_1/Day_1/Practice/Test.java
@@ -2,7 +2,7 @@ public class Test {
     public static void main (String[]args){
         // System.out.println("My name is coding dojo ");
         // System.out.println("i am 100 years old ");
-        System.out.println("My hometown is Burbank,CA ");
+        System.out.println("i am 100 years old ");
 
     }
 }
