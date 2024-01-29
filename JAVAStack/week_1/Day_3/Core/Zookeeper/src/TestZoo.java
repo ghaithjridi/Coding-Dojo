ee49ba6c5fc84acbc990e7682ae3c2f55eac8455
ghaithjridi <ghaithjridi5@gmail.com>
2024-01-29 16:25:32 +0100

    commit

---
+++ JAVAStack/week_1/Day_3/Core/Zookeeper/src/TestZoo.java
@@ -0,0 +1,30 @@
+
+public class TestZoo {
+
+	public static void main(String[] args) {
+		
+		
+		Gorilla gorrila1 = new Gorilla(0);
+		TheBat bat1 = new TheBat(300);
+		gorrila1.throwSomething();
+		gorrila1.throwSomething();
+		gorrila1.throwSomething();
+		
+		gorrila1.eatBananas();
+		gorrila1.eatBananas();
+		
+		gorrila1.climb();
+		
+		bat1.attackTown();
+		bat1.attackTown();
+		bat1.attackTown();
+		
+		bat1.fly();
+		bat1.fly();
+		
+		bat1.eatHumans();
+		
+
+	}
+
+}
