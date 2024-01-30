6d3d95b53f732ec1d3b048287c2df505dfd848f4
ghaithjridi <ghaithjridi5@gmail.com>
2024-01-30 19:21:06 +0100

    commit

---
+++ JAVAStack/week_2/Day_2/Practice/Counter/src/test/java/com/ghaith/springsession/CounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghaith.springsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
